7d2ae06a63d8bb7181ede16ac243df09ea97d596
erikfrei <eriks.freimanis@tele2.com>
2019-12-10 11:42:12 +0200

    Initial commit

---
+++ src/main/java/com/eronsdegus/yeye/YeyeApplication.java
@@ -0,0 +1,13 @@
+package com.eronsdegus.yeye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeyeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eronsdegus/yeye/YeyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eronsdegus.yeye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeyeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
